8c4d5358b32923d915cfb60139936795b9674bf5
Maria Mireles <mireles335@gmail.com>
2021-08-15 15:45:01 -0500

    render form elements

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,9 +1,14 @@
 const router = require("express").Router();
 const { Comment } = require("../../models");
 
-router.get("/", (req, res) => {});
-
-
+router.get("/", (req, res) => {
+  Comment.findAll()
+  .then(dbCommentData => res.json(dbCommentData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
 //create new post /api/comments
 router.post('/', (req, res) => {
