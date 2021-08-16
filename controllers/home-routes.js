702e15806e2c73e4c53138d314fbbdb88b191e16
Maria Mireles <mireles335@gmail.com>
2021-08-15 23:56:51 -0500

    test

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -25,7 +25,10 @@ router.get("/", (req, res) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
 
       // pass a single post object into the homepage template
-      res.render("homepage", { posts });
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch((err) => {
       console.log(err);
