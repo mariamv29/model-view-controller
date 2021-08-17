75dde121dbd9937553666958593b6add891a1300
Maria Mireles <mireles335@gmail.com>
2021-08-17 13:37:20 -0500

    comment box showing

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -45,6 +45,7 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+
 router.get('/post/:id', (req, res) => {
   Post.findOne({
     where: {
@@ -55,7 +56,6 @@ router.get('/post/:id', (req, res) => {
       'post_url',
       'title',
       'created_at',
-     
     ],
     include: [
       {
@@ -82,51 +82,7 @@ router.get('/post/:id', (req, res) => {
       const post = dbPostData.get({ plain: true });
 
       // pass data to template
-      res.render('single-post', { post });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-router.get('/post/:id', (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id
-    },
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-      
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-      }
-
-      // serialize the data
-      const post = dbPostData.get({ plain: true });
-
-      // pass data to template
+      console.log(`loggedin : ${req.session.loggedIn}`);
       res.render('single-post', {
         post,
         loggedIn: req.session.loggedIn
