b0c9fc51c0ecb46c6230d598049fb6484c7c0e7e
Maria Mireles <mireles335@gmail.com>
2021-09-28 02:14:58 -0500

    added userSeed/username-pass

---
+++ seeds/user-seeds.js
@@ -0,0 +1,70 @@
+const sequelize = require('../config/connection');
+const { User } = require("../models");
+
+
+const userdata = [
+    {
+        username: "grete1",
+        password: "642N21BaLQV"
+    },
+    {
+        username: "Farrel2",
+        password: "vmSDAiSwl6"
+    },
+    {
+        username: "Diego3",
+        password: "oIfkJXzuDxU"
+    },
+    {
+        username: "Calhoun5",
+        password: "uXOUBD72qaf"
+    },
+    {
+        username: "Felecia15",
+        password: "99PPE9KO9aqr"
+    },
+    {
+        username: "Rorie1",
+        password: "VDEAlZTYJ1"
+    },
+    {
+        username: "Emily6",
+        password: "K56cHfOzZ"
+    },
+    {
+        username: "Kalli2",
+        password: "Gybd2XR7gCcg"
+    },
+]
+
+
+
+
+
+
+
+
+
+// grete	gandrock0@thetimes.co.uk
+// Farrel	fdunsmuir1@slideshare.net
+// Diego	dduffet2@constantcontact.com
+// Rorie	rburtenshaw3@amazon.co.jp
+// Gunther	gbeeckx4@ed.gov
+// Calhoun	cravenshaw5@businessweek.com
+// Stevie	sbamblett6@netvibes.com
+// Dmitri	dlewton7@networkadvertising.org
+// Kip	ktranckle8@163.com
+// Elva	ewelland9@noaa.gov
+// Johnath	jmatsella@dyndns.org
+// Kalli	kscardb@1und1.de
+// Emily	efairsc@hexun.com
+// Raphaela	rwattersond@forbes.com
+// Barr	bhurdwelle@4shared.com
+// Felecia	fmullenderf@google.de
+// Craig	cmaydwayg@digg.com
+// Ferd	fgimbretth@parallels.com
+// Douglas	dpagitti@icio.us
+// D'arcy	dvaughnj@nps.gov
+// Si	smattsonk@technorati.com
+// Nancey	nbaultl@hp.com
+// Farley	fdurnianm@reddit.com
