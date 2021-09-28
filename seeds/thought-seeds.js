3b563c2f19704aa2bc7d21232f62b318596c6440
Maria Mireles <mireles335@gmail.com>
2021-09-28 01:37:22 -0500

    added thought_text seeds

---
+++ seeds/thought-seeds.js
@@ -0,0 +1,28 @@
+const { Thought } = require('../models');
+
+const thoughtdata =[
+    {
+        thought_text: "This is awesome",
+    },
+    {
+        thought_text: "Great post, thank you for sharing",
+    },
+    {
+        thought_text: "I'm not sure if I agree with your argument",
+    },
+    {
+        thought_text: "Finally a food blog",
+    },
+    {
+        thought_text: "This blog needs to be ran through Grammarly",
+    },
+    {
+        thought_text: "This is awesome",
+    },
+    {
+        thought_text: "I cannot point out what this post is about",
+    },
+    {
+        thought_text: "Totally agree",
+    }
+]
